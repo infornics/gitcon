45217ee9f579567e8ff5e6b09c287c47bee69c63
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-04-05 20:02:04 +0530

    chore: initial setup from revine

---
+++ src/root.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "revine";
+import { router } from "revine/routing";
+import "./styles/global.css";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
